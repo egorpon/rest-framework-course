172157d2a16bfa5fc6646e2666bcbb610329d480
egorpon <ponomarenkoegor36@gmail.com>
2025-04-23 18:41:34 +0300

    adding APIView class instead of @api_view

--- api/views.py
+++ api/views.py
@@ -6,6 +6,7 @@ from .serializers import ProductSerializer, OrderSerializer, ProductInfoSerializ
 from django.db.models import Max
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 
 
 class ProductListAPIView(generics.ListAPIView):
@@ -32,12 +33,13 @@ class UserOrderListAPIView(generics.ListAPIView):
         return qs.filter(user=self.request.user)
 
 
-@api_view(['GET'])
-def product_info(request):
-    products = Product.objects.all()
-    serializer = ProductInfoSerializer({
-        'products':products,
-        'count': products.count(),
-        'max_price': products.aggregate(max_price=Max('price'))['max_price']
-    })
-    return Response(serializer.data)
+class ProductInfoAPIView(APIView):
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductInfoSerializer({
+            'products':products,
+            'count': products.count(),
+            'max_price': products.aggregate(max_price=Max('price'))['max_price']
+            })
+        return Response(serializer.data)
+
