a7c63c671cb6c6b2a19e0dc7554908bbcb7ca05f
egorpon <ponomarenkoegor36@gmail.com>
2025-04-27 21:12:33 +0300

    adding viewset and routers, adding action, filtering and permission

--- api/filters.py
+++ api/filters.py
@@ -1,11 +1,12 @@
 import django_filters
-from .models import Product
+from .models import Product, Order
 from rest_framework import filters
 
-class InStockFilterBackend(filters.BaseFilterBackend):
 
+class InStockFilterBackend(filters.BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
         return queryset.filter(stock__gt=0)
+
 
 class ProductFilter(django_filters.FilterSet):
     class Meta:
@@ -14,3 +15,10 @@ class ProductFilter(django_filters.FilterSet):
             "name": ["iexact", "icontains"],
             "price": ["exact", "lt", "gt", "range"],
         }
+
+
+class OrderFilter(django_filters.FilterSet):
+    # created_at = django_filters.DateFilter(field_name='created_at__date')
+    class Meta:
+        model = Order
+        fields = {"status": ["exact"], "created_at": ["lt", "gt", "exact"]}
--- api/views.py
+++ api/views.py
@@ -1,16 +1,19 @@
 from django.db.models import Max
 from django.shortcuts import get_object_or_404
-from rest_framework import generics
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, generics, viewsets
 from rest_framework.decorators import api_view
+from rest_framework.pagination import LimitOffsetPagination, PageNumberPagination
 from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from api.filters import ProductFilter, InStockFilterBackend
+
+from api.filters import InStockFilterBackend, OrderFilter, ProductFilter
+
 from .models import Order, Product
 from .serializers import OrderSerializer, ProductInfoSerializer, ProductSerializer
-from rest_framework import filters
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
+
+from rest_framework.decorators import action
 
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
@@ -30,7 +33,6 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
     # pagination_class.page_query_param = "pagenum"
     # pagination_class.page_size_query_param = "size"
     # pagination_class.max_page_size = 6
-    
 
     def get_permissions(self):
         self.permission_classes = [AllowAny]
@@ -51,19 +53,36 @@ class ProductDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
         return super().get_permissions()
 
 
-class OrderListAPIView(generics.ListAPIView):
+class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.prefetch_related("items__product").all()
     serializer_class = OrderSerializer
-
-
-class UserOrderListAPIView(generics.ListAPIView):
-    queryset = Order.objects.prefetch_related("items__product").all()
-    serializer_class = OrderSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes=[IsAuthenticated]
+    pagination_class = None
+    filterset_class = OrderFilter
+    filter_backends = [
+        DjangoFilterBackend,
+    ]
 
     def get_queryset(self):
         qs = super().get_queryset()
-        return qs.filter(user=self.request.user)
+        if not self.request.user.is_staff:
+            qs = qs.filter(user=self.request.user)
+        return qs
+
+    # @action(detail=False, methods=['GET'], url_path='user-orders', )
+    # def user_orders(self, request):
+    #     orders = self.get_queryset().filter(user=request.user)
+    #     serializer = self.get_serializer(orders, many=True)
+    #     return Response(serializer.data)
+
+# class UserOrderListAPIView(generics.ListAPIView):
+#     queryset = Order.objects.prefetch_related("items__product").all()
+#     serializer_class = OrderSerializer
+#     permission_classes = [IsAuthenticated]
+
+#     def get_queryset(self):
+#         qs = super().get_queryset()
+#         return qs.filter(user=self.request.user)
 
 
 class ProductInfoAPIView(APIView):
